0eda4d456e2523478246779beedea10f97fb6eab
YangmingWen <ymnwen@ucdavis.edu>
2019-01-27 06:05:53 +0000

    [Feature] generating data label script according to the tsv + target.pth

---
+++ utils/generating_data_label.py
@@ -0,0 +1,33 @@
+import torch
+import os
+# create the reference link here
+label_file_path = '/home/yangmingwen/first_third_person/first_third_understanding/data/datasets/darknet_targets.pth'
+tsv_file_path = '/home/yangmingwen/first_third_person/first_third_understanding/data/datasets/pruned-meta.tsv'
+out_file_dir = '/home/yangmingwen/first_third_person/first_third_understanding/data/datasets/labels/'
+
+# load model
+darknet_targets = torch.load(label_file_path).type(torch.float)
+with open(tsv_file_path) as f:
+    source_link = f.readlines()
+    for index in range(0,len(source_link)):
+        # get the link
+        link = source_link[index]
+        # get the ego_link
+        ego_link = link.split('\t')[0].split('/')[-1].split('.jpg')[0] + '.txt'
+        out_file_link = out_file_dir + ego_link
+        # get the gt: cls, x_center, y_center, w, h
+        ground_truth = darknet_targets[0].tolist()
+        ground_truth = str(ground_truth).split('[')[1].split(']')[0]
+        # create the final output txt file here.
+        gt_file = open(out_file_link, 'w')
+        gt_file.write(str(ground_truth) + '\n')
+        gt_file.close()
+
+# Create the final file_list
+path = '/home/yangmingwen/first_third_person/first_third_understanding/data/datasets/images/'
+list_file = open('/home/yangmingwen/first_third_person/first_third_understanding/data/datasets/file_list_train.txt', 'w')
+for image_path in os.listdir(path):
+    list_file.write(os.path.join(path,image_path) + '\n')
+list_file.close()
+
+
