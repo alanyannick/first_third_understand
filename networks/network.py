202f1ab286f580d51df4d49f0537663e850db5f7
YangmingWen <ymnwen@ucdavis.edu>
2019-04-16 09:28:46 +0000

    [Feature] Finish the basic model including the RetinaBackBone/Regression/Classification

--- networks/network.py
+++ networks/network.py
@@ -27,9 +27,12 @@ class First_Third_Net(nn.Module):
         # class branch
         self.classifier = ClassificationModel(256, num_classes=19)
 
+        # Regression branch
+        self.regressor = RegressionModel(256)
+
         clean_weight = False
         if clean_weight:
-            for subnet in (self.exo_sfn_conv, self.ego_ss_conv, self.exo_ss_conv, self.classifier):
+            for subnet in (self.classifier, self.regressor):
                 for param in subnet.parameters():
                     if param.dim() >= 4:
                         nn.init.xavier_uniform_(param)
@@ -53,20 +56,8 @@ class First_Third_Net(nn.Module):
         ego_cat = ego_rgb
         exo_cat = exo_rgb
         # Switch for adding ss & sfn feature
-        if self.ss_feature_switch:
-            # Semantic segmentation encoder
-            ego_ss = self.semseg(self.ego_ss, return_feature_maps=False)
-            ego_ss = self.ego_ss_conv(ego_ss[0])  # The segmentation encoder returns a list of one or more tensors
-            exo_ss = self.semseg(self.exo_ss, return_feature_maps=False)
-            exo_ss = self.exo_ss_conv(exo_ss[0])  # The segmentation encoder returns a list of one or more tensors
-            # Concatenate features
-            ego_cat = torch.cat([ego_rgb, ego_ss], 1)
-            exo_cat = torch.cat([exo_rgb, exo_ss], 1)
-        if self.sfn_feature_switch:
-            # Surface normal
-            exo_sfn = self.exo_sfn_conv(self.exo_sfn)
-            exo_cat = torch.cat([exo_cat, exo_sfn], 1)
         concatted_features = torch.cat([ego_cat, exo_cat], 1)
+
         # @Verify the config file channel here
         # print(concatted_features.shape)
         # Note here, the targets dimension should be 1,1,5
@@ -76,33 +67,6 @@ class First_Third_Net(nn.Module):
         else:
             self.bbox_predict, [output, pred_conf, pred_boxes] = self.classifier(concatted_features, self.targets, self.test_mode)
             return self.bbox_predict, [output, pred_conf, pred_boxes]
-
-
-
-# class Retina_backbone(nn.Module):
-#     def __init__(self):
-#         nn.Module.__init__(self)
-#         import pickle
-#         from functools import partial
-#         pickle.load = partial(pickle.load, encoding="latin1")
-#         pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
-#         self.retinanet = torch.load('./model/coco_resnet_50_map_0_335.pt', map_location=lambda storage, loc: storage.cuda(0),
-#                                pickle_module=pickle)
-#         # self.rgb = nn.Sequential(*list(self.retinanet.children())[:-6])
-#         self.features = []
-#
-#     def forward(self, input):
-#         x = self.retinanet.conv1(input)
-#         x = self.retinanet.bn1(x)
-#         x = self.retinanet.relu(x)
-#         x = self.retinanet.maxpool(x)
-#
-#         x1 = self.retinanet.layer1(x)
-#         x2 = self.retinanet.layer2(x1)
-#         x3 = self.retinanet.layer3(x2)
-#         x4 = self.retinanet.layer4(x3)
-#         self.features = self.retinanet.fpn([x2, x3, x4])
-#         return self.features
 
 
 class ClassificationModel(nn.Module):
@@ -151,6 +115,46 @@ class ClassificationModel(nn.Module):
         out2 = out1.view(batch_size, width, height, self.num_anchors, self.num_classes)
 
         return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+
+
+class RegressionModel(nn.Module):
+    def __init__(self, num_features_in, num_anchors=9, feature_size=256):
+        super(RegressionModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act4 = nn.ReLU()
+
+        self.output = nn.Conv2d(feature_size, num_anchors * 4, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.act1(out)
+
+        out = self.conv2(out)
+        out = self.act2(out)
+
+        out = self.conv3(out)
+        out = self.act3(out)
+
+        out = self.conv4(out)
+        out = self.act4(out)
+
+        out = self.output(out)
+
+        # out is B x C x W x H, with C = 4*num_anchors
+        out = out.permute(0, 2, 3, 1)
+
+        return out.contiguous().view(out.shape[0], -1, 4)
+
 
 if __name__ == '__main__':
     net = First_Third_Net()
