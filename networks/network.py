839d2e6c1db46273e5499eb421b05089f0560aa2
YangmingWen <ymnwen@ucdavis.edu>
2019-04-17 10:28:44 +0000

    [Feature] Finish first_ego_pose_branch

--- networks/network.py
+++ networks/network.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models.resnet as resnet
+import torch.nn.functional as F
 # import model
 from torch.autograd import Variable
 from maskrcnn_benchmark.modeling.detector import build_detection_model
@@ -23,12 +24,16 @@ class First_Third_Net(nn.Module):
         self.rgb = torch.nn.Sequential(*list(self.rgb.module.children())[:-1])
         self.rgb.train()
         self.rgb = self.rgb.cuda()
-
+        # First Rranch
+        self.first_ego_pose_branch = egoFirstBranchModel(256, num_classes=19)
         # class branch
         self.classifier = ClassificationModel(256, num_classes=19)
 
         # Regression branch
         self.regressor = RegressionModel(256)
+
+        # Compress the final channel
+        # self.fc_ego_pool = nn.MaxPool2d(2,2)
 
         clean_weight = False
         if clean_weight:
@@ -43,13 +48,22 @@ class First_Third_Net(nn.Module):
         self.targets = target
         self.exo_rgb_gt = exo_rgb_gt
         # change the list of tensor to 8x3x800x800
-        self.ego_rgb = torch.stack(ego_rgb)[0]
-        self.exo_rgb = torch.stack(exo_rgb)[0]
+        self.ego_rgb = torch.stack(ego_rgb)
+        self.exo_rgb = torch.stack(exo_rgb)
 # ======================= get the feature pyramid here ==========
         with torch.no_grad():
             retina_ego_features = self.rgb(self.ego_rgb.cuda())
             retina_exo_features = self.rgb(self.exo_rgb.cuda())
-            # regression = torch.cat([self.regressionModel(feature) for feature in features], dim=1)
+
+        # for crossentropy / with out B X 1 X Class
+        first_ego_out = self.first_ego_pose_branch(retina_ego_features)
+
+
+        # ego_out = torch.cat([self.classifier(feature) for feature in retina_ego_features], dim=1)
+        # ego_out = nn.AvgPool2d((ego_out.shape[-2:]))(ego_out)
+        # F.interpolate(retina_exo_features, scale_factor=2, mode="nearest")
+        # regression = torch.cat([self.regressionModel(feature) for feature in features], dim=1)
+
 # =======================First / Second  / third branch here =========================================
         # Switch for adding ss & sfn feature
         # concatted_features = torch.cat([retina_ego_features, retina_ego_features], 1)
@@ -71,7 +85,62 @@ class egoPoseClassification(nn.Module):
         self.ego_pool = nn.AvgPool2d(13)
         self.fc = nn.Linear(255, 19)
         self.fc = nn.DataParallel(self.fc)
+        F.interpolate(num_features_in, scale_factor=2, mode="nearest")
 
+
+class egoFirstBranchModel(nn.Module):
+    def __init__(self, num_features_in, num_classes=19, prior=0.01, feature_size=256):
+        super(egoFirstBranchModel, self).__init__()
+
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
+        self.act4 = nn.ReLU()
+
+        # self.output = nn.Conv2d(feature_size, num_anchors * num_classes, kernel_size=3, padding=1)
+        self.output = nn.Conv2d(feature_size, num_classes, kernel_size=3, padding=1)
+        self.output_act = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.act1(out)
+
+        out = self.conv2(out)
+        out = self.act2(out)
+
+        out = self.conv3(out)
+        out = self.act3(out)
+
+        out = self.conv4(out)
+        out = self.act4(out)
+
+        out = self.output(out)
+        out = self.output_act(out)
+
+        # out is B x C x W x H, with C = n_classes + n_anchors
+        out1 = out.permute(0, 2, 3, 1)
+
+        batch_size, width, height, channels = out1.shape
+
+        # out2 = out1.view(batch_size, width, height, self.num_anchors, self.num_classes)
+        # out2 = out1.view(batch_size, width, height, self.num_classes)
+        # return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+
+        out2 = out1.view(batch_size, width, height, self.num_classes)
+        # out is B x WHC
+        out3 = out2.contiguous().view(x.shape[0], 1, width * height * self.num_classes)
+        # Out is 8 *1 * 19
+        out4 = nn.Linear(out3.shape[-1], self.num_classes).cuda()(out3)
+        return out4
 
 
 class ClassificationModel(nn.Module):
